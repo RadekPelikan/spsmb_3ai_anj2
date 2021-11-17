f83f4cf663f34fe6dc566b70ca0fdb295ef8e5b7
Radek Pelikan <pelikan.radek03@gmail.com>
2021-11-17 21:13:40 +0100

    forgot stuff

--- lessons/src/_11m/_15d_23/dedicnost/Uvod.java
+++ lessons/src/_11m/_15d_23/dedicnost/Uvod.java
@@ -1,5 +1,8 @@
 package _11m._15d_23.dedicnost;
-//Dědičnost -
+//Dědičnost - možnost přidat k bázové třídě,
+// základní třídě, rodičovské třídě, supertřídě, rodiči, předkovi
+// další vlastnosti a odvodit tak další (zděděnou) třídu, resp.
+// potomka, resp. dceřinnou třídu.
 public class Uvod {
 
     public static void main(String[] args) {
