fca532c8aec93c179a3e7a4d932ed68186e3c918
Radek Pelikan <pelikan.radek03@gmail.com>
2021-11-17 21:06:09 +0100

    HW and lesson

---
+++ lessons/src/_11m/_15d_23/dedicnost/Uvod.java
@@ -0,0 +1,9 @@
+package _11m._15d_23.dedicnost;
+//Dědičnost -
+public class Uvod {
+
+    public static void main(String[] args) {
+        Block a = new Block(2, 4, 6);
+        System.out.println(a.getDiagonal());
+    }
+}
