403bf9cba2aa5fd037402eda7a6305939bce035e
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-11-18 12:15:15 +0100

    lesson 18.11 PRO

---
+++ lessons/src/_11m/_18d_24/cviceniKolo/HorskeKolo.java
@@ -0,0 +1,29 @@
+package _11m._18d_24.cviceniKolo;
+
+public class HorskeKolo extends Kolo {
+    private int prevodyVzadu;
+    private int prevodyVpredu;
+
+
+    public HorskeKolo(int cena, String vyrobce, int prevodyVzadu, int prevodyVpredu) {
+        super(cena, true, vyrobce);
+        this.prevodyVzadu = prevodyVzadu;
+        this.prevodyVpredu = prevodyVpredu;
+    }
+
+    public int getPrevodyVzadu() {
+        return prevodyVzadu;
+    }
+
+    public int getPrevodyVpredu() {
+        return prevodyVpredu;
+    }
+
+    @Override
+    public String coJsiZac() {
+        return "HorskeKolo{" +
+                "prevodyVzadu=" + prevodyVzadu +
+                ", prevodyVpredu=" + prevodyVpredu +
+                '}';
+    }
+}
