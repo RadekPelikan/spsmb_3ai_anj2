6a47e030ea85c4279d7b563f16d70eb5c9d5141d
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-11-22 12:05:55 +0100

    lesson 22.11 PRO

---
+++ lessons/src/_11m/_22d_25/cviceni/c1/Usecka.java
@@ -0,0 +1,15 @@
+package _11m._22d_25.cviceni.c1;
+
+public class Usecka {
+    private int delka;
+
+    public Usecka(int delka) {
+        this.delka = delka;
+    }
+
+    public void zobraz() {
+        for (int i = 0; i < this.delka; i++) {
+            System.out.print("*");
+        }
+    }
+}
---
+++ lessons/src/_11m/_22d_25/cviceni/c2/Zadani.java
@@ -0,0 +1,11 @@
+package _11m._22d_25.cviceni.c2;
+
+import _11m._22d_25.cviceni.c1.Ctverec;
+
+//Od třídy Ctverec odvoďte třídu DutyCtverec a zajistěte správnou implementaci
+//metody void zobraz(), např:
+//      ***
+//      * *
+//      ***
+public class Zadani {
+}
