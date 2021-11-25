b5829773407f4b032762d51accfdd040260152f6
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-11-25 12:12:39 +0100

    Lesson 25.11 PRO - linked list

---
+++ lessons/src/_11m/_25d_26/SpojovySeznam.java
@@ -0,0 +1,48 @@
+package _11m._25d_26;
+
+import org.junit.runner.manipulation.Sortable;
+
+//vytvoříme jednosměrný spojový seznam (single linked list)
+// objektů, který se bude na sebe odkazovat
+//pomocí členské proměnné dalsi
+public class SpojovySeznam implements Comparable<SpojovySeznam> {
+    private int hodnota;
+    private SpojovySeznam dalsi;
+
+    public SpojovySeznam(int hodnota) {
+        this.hodnota = hodnota;
+        this.dalsi = null;
+    }
+    //najdi koncový objekt (ten, který má členskou
+    // proměnnou další null)
+    public void pridejNaKonec(SpojovySeznam obj){
+        SpojovySeznam ss = this;
+        while(ss.getDalsi() != null){
+            ss = ss.getDalsi();
+        }
+        ss.dalsi=obj;
+    }
+
+    public int getHodnota() {
+        return hodnota;
+    }
+
+    public SpojovySeznam getDalsi() {
+        return dalsi;
+    }
+
+    public int len() {
+        int i = 0;
+
+        SpojovySeznam currentE = this;
+        do{
+            i++;
+        } while((currentE=currentE.getDalsi())!=null);
+        return i;
+    }
+
+    @Override
+    public int compareTo(SpojovySeznam o) {
+        return this.hodnota - o.hodnota;
+    }
+}
---
+++ lessons/src/_11m/_25d_26/TestSS.java
@@ -0,0 +1,21 @@
+package _11m._25d_26;
+
+import java.util.Random;
+
+public class TestSS {
+    private final static int N = 1000;
+    public static void main(String[] args) {
+        Random r = new Random();
+        SpojovySeznam ss1 = new SpojovySeznam(5);
+//        ss1.pridejNaKonec(new SpojovySeznam(r.nextInt(N)));
+        for (int i = 0; i < 5; i++) {
+            ss1.pridejNaKonec(new SpojovySeznam(r.nextInt(N)));
+        }
+        SpojovySeznam currentE = ss1;
+        do{
+            System.out.format("hodnota: %s%n", currentE.getHodnota());
+        } while((currentE=currentE.getDalsi())!=null);
+        System.out.println(ss1.len());
+        System.out.println(ss1.compareTo(ss1.getDalsi()));
+    }
+}
