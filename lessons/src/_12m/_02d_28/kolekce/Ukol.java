730e69e047a40bf6e06b85d041ccd70cd61adc65
Radek Pelikán <radek.pelikan@student.spsmb.cz>
2021-12-06 12:15:59 +0100

    Lesson 06.12 PRO

--- lessons/src/_12m/_02d_28/kolekce/Ukol.java
+++ lessons/src/_12m/_02d_28/kolekce/Ukol.java
@@ -1,7 +1,32 @@
 package _12m._02d_28.kolekce;
+
+import java.util.ArrayList;
+
 //ArrayList lze vytvořit i s předem vytvořeným počtem prvků. Takto:
 //ar = new ArrayList<Integer>(N). Proveďte rychlostní test a srovnejte
 //s ar = new ArrayList<Integer> pro milión prvků;
 public class Ukol {
+    public static void main(String[] args) {
+        check(1000000);
+        check((int) (Integer.MAX_VALUE * 0.249145));
+        check(535035900);
+//        check(535035901); // CRASH
+    }
 
+    private static void check(int size) {
+        System.out.println("========================================");
+        System.out.format("%-15s%d\n", "Checking for:",  size);
+        long before;
+        float durationMs;
+
+        before = System.nanoTime();
+        var a = new ArrayList<Integer>(size);
+        durationMs = (System.nanoTime() - before)/1000000f;
+        System.out.format("%-15s%-12.6f ms\n", "With size:",  durationMs);
+
+        before = System.nanoTime();
+        var b = new ArrayList<Integer>();
+        durationMs = (System.nanoTime() - before)/1000000f;
+        System.out.format("%-15s%-12.6f ms\n", "Without size:",  durationMs);
+    }
 }
